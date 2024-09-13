522d60751a518ea828906f7708f278efe60af3c1
Marcos <marcosuba@yahoo.com.ar>
2024-09-13 20:45:20 -0300

    arreglo los estilos de las cards

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,13 +6,15 @@ import { Juego } from '../../classes/juego';
 
 import { HeaderComponent } from '../header/header.component';
 import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, RouterModule, HeaderComponent, MatCardModule],
+  imports: [CommonModule, RouterModule, HeaderComponent, MatCardModule, MatButtonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
@@ -22,9 +24,9 @@ export class HomeComponent {
     new Juego('AHORCADO', '¡Poné a prueba tu vocabulario! Adiviná la palabra antes de que se complete el'+
               'dibujo. Un clásico que te hará pensar rápido. ¿Te animás a jugar?',
               'assets/ahorcado.png'),
-    new Juego('MAYOR O MENOR', '¡Jugátela! Adiviná si la próxima carta será mayor o menor.'+
-              'Sencillo, pero súper adictivo, no vas a querer dejar de jugar. ¡Dale, animate!',
-              'assets/mayorOmenor.png'),
+    new Juego('MAYOR O MENOR', '¡Jugátela! Adiviná si la próxima carta será mayor o menor. Sencillo, '+
+              'pero súper adictivo, no vas a querer dejar de jugar con todos tus amigos. ¡Dale, animate!',
+              'assets/mayoromenor.png'),
     new Juego('PREGUNTADOS', '¿Te la pasas repitiendo frases de los simpsons? Este juego es'+
               'para vos. Demostrá que sos el que más sabe de esta familia de Springfield',
               'assets/preguntados.png'),
